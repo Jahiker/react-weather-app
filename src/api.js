02566297dc1cf83bd7c0b3140113e66af75d71f5
jahiker <rojasjahiker@gmail.com>
2022-10-30 14:52:23 -0400

    [add] geo api location service for get cities data

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8e91837f59msh1a58bfdb214820dp1a6cf1jsn6fb87a07b320',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
