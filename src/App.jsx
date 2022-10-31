1f7626d385c3c68bbfc7b6a410d39ba57d5183ad
jahiker <rojasjahiker@gmail.com>
2022-10-31 00:07:10 -0400

    [add] current weather card styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import Search from './components/search/search'
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    const forecastFecth = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFecth]).then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+
+      setCurrentWeather({ weatherResponse });
+      setForecast({ forecastResponse });
+    });
+  };
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
